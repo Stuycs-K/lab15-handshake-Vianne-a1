4c2bf0c74ae673aa1ef2a79512e1d95a0a18d314
Tiffany Yang <121731114+Vianne-a1@users.noreply.github.com>
2025-01-03 06:22:21 +0000

    stuff keeps breaking more

--- makefile
+++ makefile
@@ -1,20 +1,29 @@
-all: client server
+# Makefile for pipe networking project
+
+# Compiler and flags
+CC = gcc
+CFLAGS = -Wall -Wextra -g
+
+# Targets
+all: compile
+
+compile: client server
 
 client: basic_client.o pipe_networking.o
-	gcc -o client basic_client.o pipe_networking.o
+	$(CC) -o client basic_client.o pipe_networking.o
 
 server: basic_server.o pipe_networking.o
-	gcc -o server basic_server.o pipe_networking.o
+	$(CC) -o server basic_server.o pipe_networking.o
 
 basic_client.o: basic_client.c pipe_networking.h
-	gcc -c basic_client.c
+	$(CC) $(CFLAGS) -c basic_client.c
 
 basic_server.o: basic_server.c pipe_networking.h
-	gcc -c basic_server.c
+	$(CC) $(CFLAGS) -c basic_server.c
 
 pipe_networking.o: pipe_networking.c pipe_networking.h
-	gcc -c pipe_networking.c
+	$(CC) $(CFLAGS) -c pipe_networking.c
 
 clean:
-	rm *.o
-	rm *~
+	rm -f *.o client server
+	rm -f *~
--- pipe_networking.c
+++ pipe_networking.c
@@ -1,162 +1,160 @@
 #include "pipe_networking.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <errno.h>
 
 /*=========================
   server_setup
   =========================*/
 int server_setup() {
-  int from_client;
+    int from_client;
 
-  // Create the well-known pipe
-  if (mkfifo("WKP", 0644) == -1) {
-    perror("mkfifo");
-    exit(1);
-  }
+    // Create the well-known pipe (WKP)
+    if (mkfifo(WKP, 0644) == -1) {
+        if (errno != EEXIST) {
+            perror("mkfifo");
+            exit(1);
+        }
+    }
 
-  // Open the WKP for reading
-  from_client = open("WKP", O_RDONLY);
-  if (from_client == -1) {
-    perror("open");
-    exit(1);
-  }
+    // Open the WKP for reading
+    from_client = open(WKP, O_RDONLY);
+    if (from_client == -1) {
+        perror("open");
+        exit(1);
+    }
 
-  // Remove the WKP after connection
-  if (remove("WKP") == -1) {
-    perror("remove");
-  }
+    printf("[server] WKP created and waiting for connection...\n");
 
-  return from_client;
+    return from_client;
 }
 
 /*=========================
   server_handshake
   =========================*/
-int server_handshake(int *to_client) {
-  char private_pipe[100];
-  int from_client;
+int server_handshake(int from_client, int *to_client) {
+    char private_pipe[256];
 
-  // Read the name of the private pipe from the client
-  if (read(from_client, private_pipe, sizeof(private_pipe)) == -1) {
-    perror("read");
-    exit(1);
-  }
+    // Read the name of the private pipe from the client
+    if (read(from_client, private_pipe, sizeof(private_pipe)) <= 0) {
+        perror("read");
+        exit(1);
+    }
 
-  // Open the private pipe for writing
-  *to_client = open(private_pipe, O_WRONLY);
-  if (*to_client == -1) {
-    perror("open");
-    exit(1);
-  }
+    printf("[server] Received private pipe name: %s\n", private_pipe);
 
-  // Send acknowledgment to the client
-  if (write(*to_client, "ACK", 3) == -1) {
-    perror("write");
-    exit(1);
-  }
+    // Open the private pipe for writing
+    *to_client = open(private_pipe, O_WRONLY);
+    if (*to_client == -1) {
+        perror("open");
+        exit(1);
+    }
 
-  // Receive final acknowledgment from the client
-  char ack_response[100];
-  if (read(from_client, ack_response, sizeof(ack_response)) == -1) {
-    perror("read");
-    exit(1);
-  }
+    // Send acknowledgment to the client
+    if (write(*to_client, "ACK", 3) == -1) {
+        perror("write");
+        exit(1);
+    }
 
-  if (strcmp(ack_response, "ACK") != 0) {
-    fprintf(stderr, "Handshake failed: invalid acknowledgment from client.\n");
-    exit(1);
-  }
+    printf("[server] Sent ACK to client.\n");
 
-  return from_client;
+    // Receive final acknowledgment from the client
+    char ack_response[256];
+    if (read(from_client, ack_response, sizeof(ack_response)) <= 0) {
+        perror("read");
+        exit(1);
+    }
+
+    if (strcmp(ack_response, "ACK") != 0) {
+        fprintf(stderr, "[server] Handshake failed: invalid acknowledgment from client.\n");
+        exit(1);
+    }
+
+    printf("[server] Handshake complete.\n");
+
+    return from_client;
 }
 
 /*=========================
   client_handshake
   =========================*/
 int client_handshake(int *to_server) {
-  int to_server_fd, from_server_fd;
-  char private_pipe[100];
+    int from_server;
+    char private_pipe[256];
 
-  // Generate private pipe name
-  sprintf(private_pipe, "private_%d", getpid());
+    // Generate private pipe name
+    sprintf(private_pipe, "private_%d", getpid());
 
-  // Create the private pipe
-  if (mkfifo(private_pipe, 0644) == -1) {
-    perror("mkfifo");
-    exit(1);
-  }
+    // Create the private pipe
+    if (mkfifo(private_pipe, 0644) == -1) {
+        perror("mkfifo");
+        exit(1);
+    }
 
-  // Open the well-known pipe for writing
-  *to_server = open("WKP", O_WRONLY);
-  if (*to_server == -1) {
-    perror("open");
-    exit(1);
-  }
+    // Open the well-known pipe for writing
+    *to_server = open(WKP, O_WRONLY);
+    if (*to_server == -1) {
+        perror("open");
+        exit(1);
+    }
 
-  // Send the private pipe name to the server
-  if (write(*to_server, private_pipe, strlen(private_pipe) + 1) == -1) {
-    perror("write");
-    exit(1);
-  }
+    // Send the private pipe name to the server
+    if (write(*to_server, private_pipe, strlen(private_pipe) + 1) == -1) {
+        perror("write");
+        exit(1);
+    }
 
-  // Open the private pipe for reading
-  from_server_fd = open(private_pipe, O_RDONLY);
-  if (from_server_fd == -1) {
-    perror("open");
-    exit(1);
-  }
+    printf("[client] Sent private pipe name to server.\n");
 
-  // Read the server's acknowledgment
-  char ack[100];
-  if (read(from_server_fd, ack, sizeof(ack)) == -1) {
-    perror("read");
-    exit(1);
-  }
+    // Open the private pipe for reading
+    from_server = open(private_pipe, O_RDONLY);
+    if (from_server == -1) {
+        perror("open");
+        exit(1);
+    }
 
-  if (strcmp(ack, "ACK") != 0) {
-    fprintf(stderr, "Handshake failed: invalid acknowledgment from server.\n");
-    exit(1);
-  }
+    // Read the server's acknowledgment
+    char ack[256];
+    if (read(from_server, ack, sizeof(ack)) <= 0) {
+        perror("read");
+        exit(1);
+    }
 
-  // Send final acknowledgment to the server
-  if (write(*to_server, "ACK", 3) == -1) {
-    perror("write");
-    exit(1);
-  }
+    if (strcmp(ack, "ACK") != 0) {
+        fprintf(stderr, "[client] Handshake failed: invalid acknowledgment from server.\n");
+        exit(1);
+    }
 
-  // Remove the private pipe
-  if (remove(private_pipe) == -1) {
-    perror("remove");
-  }
+    printf("[client] Received ACK from server.\n");
 
-  return from_server_fd;
+    // Send final acknowledgment to the server
+    if (write(*to_server, "ACK", 3) == -1) {
+        perror("write");
+        exit(1);
+    }
+
+    printf("[client] Handshake complete.\n");
+
+    // Remove the private pipe
+    if (remove(private_pipe) == -1) {
+        perror("remove");
+    }
+
+    return from_server;
 }
 
 /*=========================
-  server_connect
+  multi_server_setup
   =========================*/
-int server_connect(int from_client) {
-  char private_pipe[100];
-
-  // Read the private pipe name from the client
-  if (read(from_client, private_pipe, sizeof(private_pipe)) == -1) {
-    perror("read");
-    exit(1);
-  }
-
-  // Open the private pipe for writing
-  int to_client = open(private_pipe, O_WRONLY);
-  if (to_client == -1) {
-    perror("open");
-    exit(1);
-  }
-
-  return to_client;
+int multi_server_setup() {
+    // Implementation for setting up a multi-server environment
+    int from_client = server_setup();
+    printf("[multi-server] Multi-server setup complete.\n");
+    return from_client;
 }
 
-//idk what i'm doing wrong
+/*=========================
+  multi_server_connect
+  =========================*/
+int multi_server_connect(int from_client, struct message m) {
+    // Implementation for connecting clients in a multi-server setup
+    printf("[multi-server] Received message type: %d, data: %s\n", m.type, m.data);
+    return 0; // Placeholder for actual connection logic
+}
